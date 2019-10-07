24a70366676b34401277d4f0d8e63c429aedaf16
Dmitry Voloshin <eralashpvp@gmail.com>
2019-10-07 17:51:59 +0300

    initial

---
+++ src/app/app.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+
+class App extends Component{
+    render(){
+        return <div>
+            <h2>Just initial for look</h2>
+        </div>
+    }
+}
+
+export default App;
