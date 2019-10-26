28009db922a8928fa2c375af93169c07c9ba54b8
Dmitry Voloshin <eralashpvp@gmail.com>
2019-10-26 14:29:18 +0300

    still work

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './app.css';
+import { withPhonestoreService } from '../hoc';
 
-const App = () =>{
-    return <div>Retry to Start App</div>
+const App = ({ phonestoreService }) =>{
+    console.log(phonestoreService.getPhones())
+    return <div>Working App</div>
 };
 
-export default App
+export default withPhonestoreService()(App)
